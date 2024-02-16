e3371f1827fbdeb6b46a4188ed6c370b00b363d1
Sverre Hamre <sverre.hamre@gmail.com>
2024-02-17 00:13:31 +0100

    Update the autoconnect code, did not work using the one were you set ssid and name. the anonymous ap worked so just changed to that.

--- sensiron/src/main.cpp
+++ sensiron/src/main.cpp
@@ -256,8 +256,8 @@ void setup() {
 
     bool res;
     // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("SensirionAP","sens"); // password protected ap
+    res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    //res = wm.autoConnect("SensirionAP","sens"); // password protected ap
 
     if(!res) {
         Serial.println("Failed to connect");
