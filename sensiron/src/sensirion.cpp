dd23fb7a56915f8819dc86e60fabdfdcee2b497c
Sverre Hamre <sverre.hamre@gmail.com>
2024-03-10 12:36:28 +0100

    Remove wrong include

--- sensiron/src/sensirion.cpp
+++ sensiron/src/sensirion.cpp
@@ -29,8 +29,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
-#include <arduino.h>
 #include "sensirion.h"
 
 SensirionI2CSen5x sen5x;
